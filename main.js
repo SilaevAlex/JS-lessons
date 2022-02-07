627afd2b9fd670b1f9c6cb3735718e3f772e5f30
SilaevAlex <dp110393sad@gmail.com>
2022-02-07 10:46:31 +0200

    HW2 END

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 /* first HW */
-let yourName = prompt("Enter your name")
+// let yourName = prompt("Enter your name")
 
-let result = yourName 
+// let result = yourName 
 
-alert("Hello" + result)
+// alert("Hello" + result)
 
 /* first HW  */
+
+
+
+/* HW2 Start */
+
+
+// var yourName = prompt("Enter your names")
+
+// for(var i = 0; i < yourName.length; i++) {
+//     console.log("<li>" + yourName + "</li>")
+// }
+
+
+
+var yourName = prompt("Enter your names")
+
+var arr  = yourName.split (", ")
+ for(var names of arr) {
+    console.log("<li>" + names + "</li>")
+ }
+
+ /* HW2 END */
