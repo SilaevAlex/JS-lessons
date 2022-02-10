5c5962e8d13716b12b43a42a161e3c3a7d225673
SilaevAlex <dp110393sad@gmail.com>
2022-02-10 18:50:37 +0200

    HW3

--- main.js
+++ main.js
@@ -20,11 +20,59 @@
 
 
 
-var yourName = prompt("Enter your names")
+// var yourName = prompt("Enter your names")
 
-var arr  = yourName.split (", ")
- for(var names of arr) {
-    console.log("<li>" + names + "</li>")
- }
+// var arr = yourName.split(", ")
 
- /* HW2 END */
+// for (var names of arr) {
+
+//    console.log("<li>" + names + "</li>")
+// }
+
+
+/* HW2 END */
+
+/* HW3 Start */
+
+"use strict";
+
+var phoneNumber = prompt("Enter your phone number:")
+
+var err = false
+
+
+for (var char of phoneNumber.slice(4, 13)) {
+
+    var code = char.charCodeAt()
+
+    if ( code < 48 || code > 57) {
+        err = true
+
+    }
+}
+
+if (phoneNumber[0] != "+" && err == false) {
+    alert("Please start whith '+'!")
+
+}
+
+else if (phoneNumber.length != 13) {
+    alert("Please enter '+' and 12 numbers");
+
+
+}
+else if (phoneNumber.slice(1, 4) != "380" && err == false) {
+    alert("Start number from '+380...'!");
+
+}
+
+ 
+
+else if (err == true) {
+    alert ("only num")
+}
+else{alert("It`s Ok")}
+
+
+/* HW3 End */
+
