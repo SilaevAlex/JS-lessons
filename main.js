dfbca68d116021a0012fd4436d2717f0612b5384
SilaevAlex <dp110393sad@gmail.com>
2022-02-13 13:20:22 +0200

    add  HW4

--- main.js
+++ main.js
@@ -34,45 +34,75 @@
 
 /* HW3 Start */
 
-"use strict";
-
-var phoneNumber = prompt("Enter your phone number:")
-
-var err = false
 
 
-for (var char of phoneNumber.slice(4, 13)) {
+// var phoneNumber = prompt("Enter your phone number:")
 
-    var code = char.charCodeAt()
-
-    if ( code < 48 || code > 57) {
-        err = true
-
-    }
-}
-
-if (phoneNumber[0] != "+" && err == false) {
-    alert("Please start whith '+'!")
-
-}
-
-else if (phoneNumber.length != 13) {
-    alert("Please enter '+' and 12 numbers");
+// var err = false
 
 
-}
-else if (phoneNumber.slice(1, 4) != "380" && err == false) {
-    alert("Start number from '+380...'!");
+// for (var char of phoneNumber.slice(4, 13)) {
 
-}
+//     var code = char.charCodeAt()
 
- 
+//     if ( code < 48 || code > 57) {
+//         err = true
 
-else if (err == true) {
-    alert ("only num")
-}
-else{alert("It`s Ok")}
+//     }
+// }
+
+// if (phoneNumber[0] != "+" && err == false) {
+//     alert("Please start whith '+'!")
+
+// }
+
+// else if (phoneNumber.length != 13) {
+//     alert("Please enter '+' and 12 numbers");
+
+
+// }
+// else if (phoneNumber.slice(1, 4) != "380" && err == false) {
+//     alert("Start number from '+380...'!");
+
+// }
+
+
+
+// else if (err == true) { 
+//     alert ("only num")
+// }
+// else{alert("It`s Ok")}
 
 
 /* HW3 End */
 
+
+/* HW4 start */
+
+var text = '<b>My</b> <b>name</b> is <i>Alex</i>'
+
+
+var result = ""
+
+var isFiltre = false
+
+for (var char of text) {
+
+    console.log(char)
+
+    if (char === "<") {
+        isFiltre = true
+    }
+    if (isFiltre === false) {
+        result += char
+    }
+
+    if (char === ">") {
+        isFiltre = false
+    }
+
+}
+
+alert(result)
+
+/* HW4 end */
