8b128d0d52ce6747c3c1dd26e5c0a2e1c5cdcbe1
SilaevAlex <dp110393sad@gmail.com>
2022-02-19 22:34:53 +0200

    add HW3

--- HWfunction.js
+++ HWfunction.js
@@ -34,24 +34,24 @@ console.log(res) // <p>Hello world</p> */
 
 // hw2 START 
 
-function exponentiate(num, ext) {
+// function exponentiate(num, ext) {
 
-    var sum = []
-
-
-    for (var i = 0; i < num.length; i++) {
-        if (num[i] **= ext) {
-            sum.push(num[i]);
-        }
-    }
-
-    return sum
-}
+//     var sum = []
 
 
-var res = exponentiate([2, 4, 3, 1], 2)
+//     for (var i = 0; i < num.length; i++) {
+//         if (num[i] **= ext) {
+//             sum.push(num[i]);
+//         }
+//     }
 
-console.log(res)
+//     return sum
+// }
+
+
+// var res = exponentiate([2, 4, 3, 1], 2)
+
+// console.log(res)
 
 // Написать функцию которая:
 
@@ -67,3 +67,40 @@ console.log(res)
 
 
 // Результат: [4, 16, 9, 1]
+
+// hw end 
+
+
+// hw3 START 
+
+function sort(str) {
+
+    var strRev = [];
+
+    for (var i = str.length - 1; i >= 0; i--) {
+        strRev.push(str[i]);
+    }
+    return strRev
+}
+
+
+
+
+var res = sort(["a", "b", "c"])
+
+console.log(res)
+// Написать функцию которая:
+
+
+//     принимает массив значений ["a", "b", "c"]
+//     возращает новый массив из значений отсортированных наоборот
+
+
+// Пример:
+
+// 	var res = sort(["a", "b", "c"])
+
+
+// 	Результат: ["c", "b", "a"]
+
+// HW END
