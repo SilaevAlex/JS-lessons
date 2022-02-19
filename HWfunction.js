281bdd17c39c5366b9e48939ff736185edbbdb52
SilaevAlex <dp110393sad@gmail.com>
2022-02-19 21:35:51 +0200

    add hw2

--- HWfunction.js
+++ HWfunction.js
@@ -1,16 +1,16 @@
 'use strict'
 
+// hw1 start
+// function wrapTags(str, tag) {
+//     return `<${tag}>${str}</${tag}>`
 
-function wrapTags(str, tag) {
-    return `<${tag}>${str}</${tag}>`
-
-}
-
-
-var res = wrapTags("Hello world", "p")
+// }
 
 
-console.log(res) // <p>Hello world</p> */
+// var res = wrapTags("Hello world", "p")
+
+
+// console.log(res) // <p>Hello world</p> */
 
 
 /* Написать фунцию:
@@ -29,3 +29,41 @@ var res = wrapTags("Hello world", "p")
 
 
 console.log(res) // <p>Hello world</p> */
+
+// hw1 end 
+
+// hw2 START 
+
+function exponentiate(num, ext) {
+
+    var sum = []
+
+
+    for (var i = 0; i < num.length; i++) {
+        if (num[i] **= ext) {
+            sum.push(num[i]);
+        }
+    }
+
+    return sum
+}
+
+
+var res = exponentiate([2, 4, 3, 1], 2)
+
+console.log(res)
+
+// Написать функцию которая:
+
+
+
+//     принимает массив чисел [2, 4, 3, 1] и степень числа 2
+//     возращает новый массив из чисел возведенных в указанную степень
+
+
+// Пример:
+
+// var res = exponentiate([2, 4, 3, 1], 2)
+
+
+// Результат: [4, 16, 9, 1]
